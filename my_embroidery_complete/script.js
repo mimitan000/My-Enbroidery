618f644628b6907e5f3f9fa63ac88f81b59999c3
mimitan000 <miki.ask.0917@gmail.com>
2025-08-04 05:17:18 +0900

    Delete my_embroidery_complete directory

--- my_embroidery_complete/script.js
+++
@@ -1,7 +0,0 @@
-
-function showTab(tabId) {
-  const tabs = document.querySelectorAll('.tab-content');
-  tabs.forEach(tab => tab.style.display = 'none');
-  document.getElementById(tabId).style.display = 'block';
-  console.log(`Switched to tab: ${tabId}`);
-}
